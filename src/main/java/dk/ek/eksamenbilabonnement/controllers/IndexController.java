bbef9f9ea2c876674799c6e070310e3c71403ff9
loegcas <110066175+4shizzgndlf@users.noreply.github.com>
2026-04-24 18:06:33 +0200

    first commit

---
+++ src/main/java/dk/ek/eksamenbilabonnement/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package dk.ek.eksamenbilabonnement.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
+    }
+}
---
+++ src/test/java/dk/ek/eksamenbilabonnement/EksamenbilabonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.eksamenbilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenbilabonnementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
